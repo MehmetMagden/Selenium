5bb7ea91ac96f2f05e4fbb5f8c78278b3e229dcf
Mehmet Magden <yasamtr@gmail.com>
2022-05-10 22:19:00 +0300

    BackUpp

--- src/test/java/day02_driverMethodlari/C06_Navigate.java
+++ src/test/java/day02_driverMethodlari/C06_Navigate.java
@@ -4,8 +4,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class C06_Navigate {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        /*
+            Eğer testimiz sirasında birden fazla sayfa arasında ileri geri hareket edeceksek driver.get() yerine driver.navigate.to() method'unu kullanırız.
+            ve sonrasında forward(), back() veya refresh() methodlarını kullanabiliriz
+         */
         System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.amazon.com");
+        Thread.sleep(2000);
+        driver.navigate().to("https://www.facebook.com");
+        Thread.sleep(2000);
+
+        //amazon'a geri dönelim
+        driver.navigate().back();
+        Thread.sleep(2000);
+
+        //yeniden facebook'a gidelim
+        driver.navigate().forward();
+        Thread.sleep(2000);
+
+        //sayfayi refresh yapalım
+
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+
+        driver.close();
     }
 }
